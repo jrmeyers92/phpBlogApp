e3f5e8a7cf89b831859d32e806b632bc7777f701
Jake Meyers <JakeRMeyers92@gmail.com>
2021-12-03 13:49:43 -0600

    core class complete for routing

--- App/bootstrap.php
+++ App/bootstrap.php
@@ -1,1 +1,6 @@
-saa dude
+<?php
+
+//Load Libraries
+    require_once "libraries/Core.php";
+    require_once "libraries/Controller.php";
+    require_once "libraries/Database.php";
--- Public/index.php
+++ Public/index.php
@@ -1,2 +1,6 @@
 <?php 
     require_once "../app/bootstrap.php";
+
+    // init Core Library 
+
+    $init = new Core;
